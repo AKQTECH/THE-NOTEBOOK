dcc282483acc845dcc9069fb5839c8c234571494
AKQTECH <aprilquioh@gmail.com>
2026-04-29 21:29:30 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Caches app shell + Google Fonts so it opens offline once installed.
    Bump CACHE_VERSION to force a refresh on deploy. */
 
-const CACHE_VERSION = 'notebook-v19';
+const CACHE_VERSION = 'notebook-v21';
 const APP_SHELL = [
   './',
   './index.html',
